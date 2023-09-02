665cc65e9f61509f217def6878f595dc074e3c86
Daniil <Soboliev.danik@gmail.com>
2023-09-02 11:36:03 +0300

    Add queries

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -137,14 +137,175 @@ namespace second_
             UpdateCollection(query);
         }
 
-        private void Btn5_Click(object sender, RoutedEventArgs e)
+        private void Btn5_HW_Click(object sender, RoutedEventArgs e)
         {
+            // ФИО - второй отдел
             IEnumerable<Pair> query = dataContext.Managers.Join(
                     dataContext.Departments,
                     m => m.IdSecDep,
                     d => d.Id,
                     (m, d) => new Pair() { Key = $"{m.Surname} {m.Name[0]}.{m.Secname[0]}.", Value = d.Name }
                 ).OrderBy(pair => pair.Value);
+
+            UpdateCollection(query);
+        }
+
+
+        #region Генератор - увеличивается при каждом обращении
+        private int _N;
+        public int N { get => _N++; set => _N = value; }
+        #endregion
+        private void Btn6_Click(object sender, RoutedEventArgs e)
+        {
+            // порядковий номер отдела - название отдела
+            // Неправильное решение (получаем только одинаковые числа для каждой записи)
+            N = 1;
+            IQueryable query = dataContext.Departments
+                .OrderBy(d => d.Name)
+                .Select(  // ранее связывание
+                    d => new Pair() { Key = N.ToString(), Value = d.Name }
+                );  // до N имеется одноразовое обращение при построении SQL запроса
+                    // не многоразовое обращение
+
+            // Pair в Select для того чтобы просто указать что хотим получить
+
+            UpdateCollection(query);
+
+            /*  Как это работает?
+                var query = ... задаёт правило, из чего будет построенный SQL
+                Какой SQL будет в результате?
+                   SELECT 1, d.Name FROM Departments d ORDER BY d.Name
+                   при формировании запроса EF 'увидет', что к него входит переменная
+                   N, он её вычеслит и добавит в SQL - в запросе будет значение N (1)
+                Итерация - по результатам запроса
+                   1 - Бухгалтерия
+                   1 - Отдел кадров
+                   ...
+                Объекты Pair создаются из этих результатов, и имеют "1" в ключе
+
+                Это ранее связывание переменной, при подготовке запроса и перед выполнением запроса
+                Поэтому N - это 1 и остаётся.
+                N(1)  --> SQL --> рез-таты с (1), а не с N
+            */
+        }
+
+        private void Btn7_Click(object sender, RoutedEventArgs e)
+        {
+            // порядковий номер отдела - название отдела
+            // Правильное решение
+            N = 1;  // инициализация счётчика(генератора)
+            IEnumerable<Pair> query = dataContext.Departments
+                .OrderBy(d => d.Name)
+                .AsEnumerable()  // преобразователь, далее LINQ-Enumerable, также можно .ToList()
+                .Select(         // ToList() - это создать List<> в памяти, а Enumerable это правило(генератор)
+                    d => new Pair() {  // позднее связывание
+                        Key = N.ToString(),  // в режиме LINQ-Enumerable это действительно
+                        Value = d.Name       // повторяется и 'N' увеличивается
+                    }
+                );
+
+            UpdateCollection(query);
+
+            /*  Как это работает? Отличия от Button6
+                Запрос SQL формируется инструкциями, которые передают .AsEnumerable()
+                .Departments
+                .OrderBy(d => d.Name)  ->  SELECT 1, d.Name FROM Departments d ORDER BY d.Name
+                Запрос выполняется с результатами:
+                   id - Бухгалтерия
+                   id - Отдел кадров
+                Итерация в цикле создаёт объекты Pair в которых есть обращение к N и с каждым
+                обращением N увеличивается в геттере генератора N.
+                Это позднее связывание
+
+                (*)  -->  SQL  -->  рез-таты с N
+             */
+        }
+
+        private void Btn8_Click(object sender, RoutedEventArgs e)
+        {
+            // название отдела - кол-во работников
+            // GroupJoin - аналог GROUP BY
+
+            IQueryable query = dataContext.Departments  // левая таблица
+                .GroupJoin(
+                    dataContext.Managers,  // правая таблица
+                    d => d.Id,  // первичный ключ (с одним значениям)
+                    m => m.IdMainDep,  // внешний ключ (с множеством значений)
+                    (d, m) => new Pair { Key = d.Name, Value = m.Count().ToString() }
+                    // группа: один отдел - коллекция работников
+                    // к первому параметру обращаемся как к объекту (один объект)
+                    // ко второму - как к коллекции (IEnumerable)
+                ).OrderByDescending(p => Convert.ToInt32(p.Value));
+
+            UpdateCollection(query);
+        }
+
+        private void Btn9_Click(object sender, RoutedEventArgs e)
+        {
+            // 1. ФИО - кол-во подчиненных
+            // 2. ФИО - кол-подчиннёных (но убрать записи где подчинённых >= 2)
+            IQueryable query = dataContext.Managers.GroupJoin(  // шеф
+                    dataContext.Managers,  // подчинённый
+                    chief => chief.Id,
+                    m => m.IdChief,
+                    (chief, m) => new Pair { Key = $"{chief.Surname} {chief.Name[0]}.{chief.Secname[0]}.", Value = m.Count().ToString() }
+                ).Where(pair => Convert.ToInt32(pair.Value) >= 2);
+            UpdateCollection(query);
+        }
+
+        private void Btn10_Click(object sender, RoutedEventArgs e)
+        {
+            // найти однофамильцев
+            N = 1;
+            IEnumerable<Pair> query = dataContext.Managers.GroupBy(
+                    m => m.Surname
+                )
+                // group.Key -> m => m.Surname
+                .Select(group => new Pair { Key = group.Key, Value = group.Count().ToString() })
+                .Where(p => p.Value != "1");
+                //.Select(p => new Pair { Key = N.ToString(), Value = p.Key });  // добавил для ДЗ
+
+            UpdateCollection(query);
+        }
+
+        private void Btn11_HW_Click(object sender, RoutedEventArgs e)
+        {
+            // название отдела - количество совместителей (SecDep)
+            IQueryable query = dataContext.Departments.GroupJoin(
+                    dataContext.Managers,
+                    d => d.Id,
+                    m => m.IdSecDep,
+                    (d, m) => new Pair { Key = d.Name, Value = m.Count().ToString() }
+                )
+                .OrderByDescending(pair => Convert.ToInt32(pair.Value));
+
+            UpdateCollection(query);
+        }
+
+        private void Btn12_HW_Click(object sender, RoutedEventArgs e)
+        {
+            // запрос с однофамильцами с нумерацией
+            N = 1;  // генератор
+            IEnumerable<Pair> query = dataContext.Managers
+                .GroupBy(m => m.Surname)
+                .AsEnumerable()
+                .Where(m => m.Count() > 1)
+                .Select(group => new Pair { Key = N.ToString(), Value = group.Key });
+
+            UpdateCollection(query);
+        }
+
+        private void Btn13_HW_Click(object sender, RoutedEventArgs e)
+        {
+            // вывести трех сотрудников с наибольшим количеством подчиненных: кол-во подчиненных - Ф И.О.
+            IQueryable query = dataContext.Managers.GroupJoin(
+                    dataContext.Managers,
+                    chief => chief.Id,
+                    m => m.IdChief,
+                    (chief, m) => new Pair { Key = m.Count().ToString(), Value = $"{chief.Surname} {chief.Name[0]}.{chief.Secname[0]}." }
+                )
+                .OrderByDescending(pair => Convert.ToInt32(pair.Key))
+                .Take(3);
 
             UpdateCollection(query);
         }
